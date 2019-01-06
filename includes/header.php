212eea9f123771b19713510fca4cdc1b09b6691f
Ranathunga <URanaT1@PEROOT.COM>
2019-01-06 16:43:53 +0530

    first commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+	<title>Mobile.LK</title>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+		<div class="navbar navbar-default navbar-fixed-top" id="topnav">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand">Mobile.LK</a>
+			</div>
+
+
+		</div>
+	</div>
+</head>
